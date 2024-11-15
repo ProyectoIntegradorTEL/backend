f4e9593444785f3e89972b621051d36c36a7b800
Ricardo965 <ricardiox2000@gmail.com>
2024-11-15 09:17:01 -0500

    fix: properties fix

--- src/main/java/com/example/pdsbackend/config/WebSecurityConfig.java
+++ src/main/java/com/example/pdsbackend/config/WebSecurityConfig.java
@@ -25,80 +25,85 @@ import org.springframework.web.filter.CorsFilter;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtRequestFilter jwtRequestFilter;
+        private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+        private final JwtRequestFilter jwtRequestFilter;
 
-    public WebSecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-                             JwtRequestFilter jwtRequestFilter) {
-        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-        this.jwtRequestFilter = jwtRequestFilter;
-    }
+        public WebSecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                        JwtRequestFilter jwtRequestFilter) {
+                this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+                this.jwtRequestFilter = jwtRequestFilter;
+        }
 
-    /*
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // configure AuthenticationManager so that it knows from where to load
-        // user for matching credentials
-        // Use BCryptPasswordEncoder
-        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-    }*/
+        /*
+         * @Autowired
+         * public void configureGlobal(AuthenticationManagerBuilder auth) throws
+         * Exception {
+         * // configure AuthenticationManager so that it knows from where to load
+         * // user for matching credentials
+         * // Use BCryptPasswordEncoder
+         * auth.userDetailsService(jwtUserDetailsService).passwordEncoder(
+         * passwordEncoder());
+         * }
+         */
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+                        throws Exception {
+                return authenticationConfiguration.getAuthenticationManager();
+        }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173"); // Cambia si tu frontend está en otro puerto
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+        @Bean
+        public CorsFilter corsFilter() {
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowCredentials(true);
+                config.addAllowedOrigin("http://localhost:5173"); // Cambia si tu frontend está en otro puerto
+                config.addAllowedHeader("*");
+                config.addAllowedMethod("*");
+                source.registerCorsConfiguration("/**", config);
+                return new CorsFilter(source);
+        }
 
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+                /*
+                 * httpSecurity
+                 * .csrf(csrf -> csrf.disable()) // Actualización para CSRF
+                 * // .cors().configurationSource(corsConfigurationSource()).and()
+                 * // dont authenticate this particular request
+                 * .authorizeRequests().requestMatchers("/authenticate").permitAll().
+                 * // all other requests need to be authenticated
+                 * anyRequest().authenticated().and().
+                 * // make sure we use stateless session; session won't be used to
+                 * // store user's state.
+                 * exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and
+                 * ().sessionManagement()
+                 * .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                 */
+                httpSecurity
+                                .csrf(csrf -> csrf.disable())
+                                .cors(Customizer.withDefaults())
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers("/auth/**", "/patient/**", "/evaluation/**",
+                                                                "/evaluation-types/**")
+                                                .permitAll()
+                                                .anyRequest().authenticated())
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .exceptionHandling(exceptions -> exceptions
+                                                .authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                                .sessionManagement(Customizer.withDefaults());
+                ;
 
-        /*httpSecurity
-                .csrf(csrf -> csrf.disable()) // Actualización para CSRF
-                // .cors().configurationSource(corsConfigurationSource()).and()
-                // dont authenticate this particular request
-                .authorizeRequests().requestMatchers("/authenticate").permitAll().
-                // all other requests need to be authenticated
-                        anyRequest().authenticated().and().
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
-                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
-        httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**", "/patient/**", "/evaluation/**", "/evaluation-types/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .exceptionHandling(exceptions -> exceptions
-                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                )
-                .sessionManagement(Customizer.withDefaults());
-        ;
+                // Add a filter to validate the tokens with every request
+                httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
-        // Add a filter to validate the tokens with every request
-        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return httpSecurity.build();
-    }
+                return httpSecurity.build();
+        }
 }
